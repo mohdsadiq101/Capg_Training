a03f6eb29f45d0b9a9ab45037f9128116eb2cf3c
Mohd Sadique <mohdsadique757@gmail.com>
2026-03-17 02:11:20 +0530

    Add files via upload

---
+++ Spring(15_March)-SpringBoot/first-app/src/main/java/com/cg/bean/Address.java
@@ -0,0 +1,38 @@
+package com.cg.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address {
+    @Value("${city}")
+    private String city;
+    @Value("${country}")
+    private String country;
+    @Value("${zip}")
+    private long zip;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(long zip) {
+        this.zip = zip;
+    }
+}
---
+++ Spring(15_March)-SpringBoot/first-app/src/main/java/com/cg/bean/Employee.java
@@ -0,0 +1,62 @@
+package com.cg.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+    @Value("${eid}")
+    private int empid;
+    @Value("${name}")
+    private String name;
+    //field injection
+//    @Autowired
+//    @Qualifier("getBean3") // qualifier is not good practice here so we use constructor injection
+    private Address address;
+
+//    public Employee(){}
+
+    // constructor injection
+    // we do not use multiple constructors in constructors injection
+    //@Autowired // not required since spring 4.0
+    public Employee(Address address) {
+        this.address = address;
+    }
+
+    // since it is required to write @Autowired in field injection but not in constructor injection that's why
+    // field inj. is not a good practice, cons. inj. is
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void printEmployeeDetails(){
+        System.out.println("Employee ID:"+empid);
+        System.out.println("Employee Name:"+name);
+        System.out.println("====Address====");
+        System.out.println("City: "+address.getCity());
+        System.out.println("Country: "+address.getCountry());
+        System.out.println("Zip: "+address.getZip());
+    }
+}
