40ae847ae76a7ef4bdc71a765ec6a7a117f475cd
Mohd Sadique <mohdsadique757@gmail.com>
2026-03-23 21:55:02 +0530

    Add files via upload

---
+++ spring-rest-demo-mockito/src/main/java/com/cg/entity/Employee.java
@@ -0,0 +1,55 @@
+package com.cg.entity;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int empid;
+	private String name;
+	private LocalDate dob;
+	private Double salary;
+	
+	public Employee() {
+	}
+	public Employee(String name, LocalDate dob, Double salary) {
+		super();
+		this.name = name;
+		this.dob = dob;
+		this.salary = salary;
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
