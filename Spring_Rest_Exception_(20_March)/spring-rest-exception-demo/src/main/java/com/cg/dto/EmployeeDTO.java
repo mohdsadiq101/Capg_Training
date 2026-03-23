a2dad81f9761bd4b6cbe91e7ac4224eae1f8b917
Mohd Sadique <mohdsadique757@gmail.com>
2026-03-23 11:10:52 +0530

    Add files via upload

---
+++ Spring_Rest_Exception_(20_March)/spring-rest-exception-demo/src/main/java/com/cg/dto/EmployeeDTO.java
@@ -0,0 +1,56 @@
+package com.cg.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class EmployeeDTO {
+	private int employeeId;
+	@NotNull(message = "Name is required")
+	@NotBlank(message = "Enter valid name")
+	private String fullName;
+	@JsonFormat(pattern = "dd-MMM-yyyy")
+	private LocalDate dateOfBirth;
+	@NotNull(message = "Salary is required")
+	@Min(value = 10000)
+	private Double salary;
+	
+	public EmployeeDTO() {
+	}
+
+	public EmployeeDTO(String fullName, LocalDate dateOfBirth, Double salary) {
+		super();
+		this.fullName = fullName;
+		this.dateOfBirth = dateOfBirth;
+		this.salary = salary;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
